252ba5eab01f94914664bf58802e5c2b22015e70
agust <agustin198ar@gmail.com>
2025-08-21 16:20:10 -0300

    dom

---
+++ Dominio/BrandEntity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    internal class BrandEntity
+    {
+        private string _name;
+        public int? Id { get; private set; } //INT? tipo nulleable
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)) //NO ESTE VACIO, NO ESPACIO, NO NULL
+                {
+                    throw new ArgumentException("Name cannot be null or empty.", nameof(value));
+                }
+                _name = value;
+            }
+        }
+        public BrandEntity(string name) //SOLO NOMBRE
+        {
+            Name = name;
+        }
+        public BrandEntity(int? id, string name)
+        {
+            if(id <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id), "Id debe ser positivo. ");
+            }
+        }
+        public override string ToString()
+        {
+            return $"BrandEntity: Id={Id}, Name={Name}";
+        }
+    }
+}
