866993cba660046feea9477ad42b4499ee828d1a
agust <agustin198ar@gmail.com>
2025-08-21 16:44:21 -0300

    referencias a dom

---
+++ Application/Brand/UseCases/BrandUseCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Abstractions;
+
+namespace Application.Brand.UseCases
+{
+    public  class BrandUseCase : iUserCase //orquestador - caso de uso - servicio
+    {
+
+    }
+}
