b3767498a0833b2bf8532d512afacf482e5e7188
agust <agustin198ar@gmail.com>
2025-08-21 16:46:38 -0300

    BrandUseCase

--- Application/Brand/UseCases/BrandUseCase.cs
+++ Application/Brand/UseCases/BrandUseCase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Domain;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +8,12 @@ using Application.Abstractions;
 
 namespace Application.Brand.UseCases
 {
-    public  class BrandUseCase : iUserCase //orquestador - caso de uso - servicio
+    public  class BrandUseCase : IUserCase<BrandEntity> //orquestador - caso de uso - servicio
     {
-
+        private IRepositoryz<BrandEntity> _repository;
+        public BrandUseCase(IRepositoryz<BrandEntity> repository) //Inyeccion de dependencias
+        {
+           => _repository = repository;
+        }
     }
 }
