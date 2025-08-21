5cb707f6791048d52089b528ab1c8ce34b4b4afb
agust <agustin198ar@gmail.com>
2025-08-21 16:25:46 -0300

    interfaz

---
+++ Application/Abstractions/iUserCase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstractions
+{
+    public interface iUserCase<TEntity> //Contrato
+    {
+        Task<TEntity> GetByIdAsync(int id); //Asincronia
+        Task<IEnumerable<TEntity>> GetAllAsyncs(int id); 
+        Task AddAssync(TEntity entity);
+        Task Update(TEntity entity);
+        Task Delete(int id);
+    }
+}
